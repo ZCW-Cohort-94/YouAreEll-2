0ba4c820fe497fd3623e98b513c7582fc288dafe
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-08 21:06:54 -0400

    all commands work, janky and wrong but they sure do work

--- Client/src/main/java/youareell/YouAreEll.java
+++ Client/src/main/java/youareell/YouAreEll.java
@@ -13,65 +13,68 @@ public class YouAreEll {
 
     TransactionController tt;
 
-    public YouAreEll (TransactionController t) {
+    public YouAreEll(TransactionController t) {
         this.tt = t;
     }
 
-    public YouAreEll (MessageController m, IdController i){
-        this(new TransactionController(m,i));
+    public YouAreEll(MessageController m, IdController i) {
+        this(new TransactionController(m, i));
     }
 
     public static void main(String[] args) throws IOException {
         // hmm: is this Dependency Injection?
         YouAreEll urlhandler = new YouAreEll(
-            new TransactionController(
-                new MessageController(), new IdController()
-        ));
-//        System.out.println(urlhandler.makeURLCall("/ids", "GET", ""));
-//        System.out.println(urlhandler.makeURLCall("/messages", "GET", ""));
+                new TransactionController(
+                        new MessageController(), new IdController()
+                ));
     }
 
-//    private String makeURLCall(String area, String type, String extra) throws IOException {
-//        URL url = new URL("http://zipcode.rocks:8085" + area);
-//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//        con.setRequestMethod(type);
-//
-//        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//
-//        String inputLine;
-//        StringBuilder s = new StringBuilder();
-//
-//        while ((inputLine = in.readLine()) != null) {
-//            s.append(inputLine).append("\n");
-//        }
-//        in.close();
-////        System.out.println(s);
-//        return s.toString();
-//    }
 
     public String get_ids() {
         try {
-            return tt.makecall("/ids", "GET", "");
+            return tt.get("/ids");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String get_messages() throws IOException {
+    public String get_messages() {
         try {
-            return tt.makecall("/messages", "GET", "");
+            return tt.get("/messages");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String send_all(String id, String message){
+    public String send_all(String id, String message) {
         try {
-            return tt.makecall("/ids/" + id + "/messages", "POST", message);
+            return tt.send_all("/ids/" + id + "/messages", message);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public String send_to(String id, String message, String toId) {
+        try {
+            return tt.send_to("/ids/" + id + "/messages", "POST", message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public String get_usr_message(String id) {
+        try {
+            return tt.get("/ids/" + id + "/messages");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String register(String name, String id) {
+        try {
+            return tt.register(name, id);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
