5ab94b1f0036b5d9a5929ad2b8ed7f166f73d256
Seth <seth@DavidszeMacBook.localdomain>
2023-04-09 13:59:45 -0400

    one of the files did not commit or something? not sure what happened

--- Client/src/main/java/controllers/IdController.java
+++ Client/src/main/java/controllers/IdController.java
@@ -23,7 +23,7 @@ public class IdController {
     public Id postId(Id id) {
         // create json from id
         // call server, get json result Or error
-        // result json to Id obj
+        // result json to Id obj"
 
         return null;
     }
